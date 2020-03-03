9bdd1a459a2c78bdee9857f522af05809efc65ba
Stephan Thober <stephan.thober@ufz.de>
2020-03-03 17:20:53 +0100

    modified if statement for the case when dem is read from nc

--- src/setVario.f90
+++ src/setVario.f90
@@ -111,7 +111,6 @@ subroutine dMatrix
   integer(i4)                     :: i, j, k
   integer(i4)                     :: r, c, ii, jj
   integer(i4)                     :: delta, nTcell
-  integer(i4)                     :: NoCellsFiner
   real(dp)                        :: xc, yc
   integer(i4), allocatable        :: list(:)
   !
@@ -159,32 +158,34 @@ subroutine dMatrix
   !
   r=1
   c=0
-  NoCellsFiner = (gridMeteo%cellsize / grid%cellsize) **2 
-  xc = gridMeteo%xllcorner + dble(gridMeteo%cellsize) * 0.5_dp
+  if (DEMNcFlag /= 1) xc = gridMeteo%xllcorner + dble(gridMeteo%cellsize) * 0.5_dp
   delta = cellFactor / 2
   jj = delta
   do k=1,nCell 
- 
-    if (r == 1) then
-       c = c + 1
-       if (c > 1) then 
-         xc = xc + dble(gridMeteo%cellsize)
-         jj = jj + cellFactor
-       end if
-       yc = gridMeteo%yllcorner + dble(gridMeteo%cellsize) * (dble(gridMeteo%nrows) - 0.5_dp)
-       ii = delta 
-    else
-       yc = yc - dble(gridMeteo%cellsize)
-       ii = ii + cellFactor
-    end if
-    print *,"DEMNcFlag: ",DEMNcFlag
-    if (DEMNcFlag == 1) then
-       cell(k)%x = gridMeteo%easting(r,c)
-       cell(k)%y = gridMeteo%northing(r,c)
-    else 
-    cell(k)%x = xc
-    cell(k)%y = yc
-    end if
+
+     if (r == 1) then
+        c = c + 1
+     end if
+
+     print *,"DEMNcFlag: ",DEMNcFlag
+     if (DEMNcFlag == 1) then
+        cell(k)%x = gridMeteo%easting(r,c)
+        cell(k)%y = gridMeteo%northing(r,c)
+     else 
+        if (r == 1) then
+           if (c > 1) then 
+              xc = xc + dble(gridMeteo%cellsize)
+              jj = jj + cellFactor
+           end if
+           yc = gridMeteo%yllcorner + dble(gridMeteo%cellsize) * (dble(gridMeteo%nrows) - 0.5_dp)
+           ii = delta 
+        else
+           yc = yc - dble(gridMeteo%cellsize)
+           ii = ii + cellFactor
+        end if
+        cell(k)%x = xc
+        cell(k)%y = yc
+     end if
 
     ! average of only four DEM cells around centre cell (from lower grid scale upto higher grid cell)
     !cell(k)%h = 0.25_dp*(G(ii,jj)%h + G(ii,jj+1)%h + G(ii+1,jj)%h + G(ii+1,jj+1)%h)
@@ -196,10 +197,8 @@ subroutine dMatrix
     else
       cell(k)%h  = sum(G( (ii-delta+1):(ii+delta) , (jj-delta+1):(jj+delta) )%h, &
                        G( (ii-delta+1):(ii+delta) , (jj-delta+1):(jj+delta) )%h /= gridMeteo%nodata_value ) / dble(nTcell)
-      if ( NoCellsFiner < NoCellsFiner) then
-         print*, 'Cells with non matching DEM on the finer scale (nodata values in finer grid)'
-      end if
     end if
+
     r=r+1
     if (r > gridMeteo%nrows) r = 1
     ! MZMZMZMZ - delete !!!!!!!
