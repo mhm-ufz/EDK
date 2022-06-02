a6abe05a0bb571396cdbc8e0ada0c442c768ace3
Sebastian Müller <mueller.seb@posteo.de>
2022-06-02 09:50:40 +0200

    prepare forces

---
+++ doc/CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All notable changes to will be documented in this file.
+
+
+## v0.2.0 - ?
+
+### Enhancements
+
+### Changes
+
+### Bugfixes
+
+## v0.1.0 - ?
+
+### Enhancements
+
+### Changes
+
+### Bugfixes
---
+++ src/mo_edk_info.F90
@@ -0,0 +1,35 @@
+!> \file    mo_edk_info.f90
+!> \copydoc mo_edk_info
+
+#ifndef EDKVERSION
+#define EDKVERSION "0.0.0-dev0"
+#endif
+#ifndef EDKDATE
+#define EDKDATE __DATE__
+#endif
+#define set_version(x) character(len = *), parameter :: version = x
+#define set_date(x) character(len = *), parameter :: version_date = x
+
+!> \brief   module with edk program information
+!> \details Provides information about the edk program as parameters.
+!!          The \p version parameter will be set during compilation
+!!          to content of \a version.txt.
+!!          The \p version_date parameter will be set during compilation
+!!          to content of \a version_date.txt,
+!!          if it is a release version, otherwise it will be the current date.
+!> \authors Sebastian Mueller
+!> \date    May 2022
+module mo_edk_info
+
+  implicit none
+
+  set_version(EDKVERSION)
+  !< Current edk version
+
+  set_date(EDKDATE)
+  !< Time of current edk version release
+
+  !> Driver file
+  character(len = *), parameter :: file_main = 'EDK_driver.f90'
+
+end module mo_edk_info
