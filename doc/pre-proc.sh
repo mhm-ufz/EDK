410bc7ac4a650f18e4f484740bf1f992a1a6e52b
Sebastian Müller <sebastian.mueller@ufz.de>
2022-06-22 14:46:03 +0200

    doc: pre-proc fix

--- doc/pre-proc.sh
+++ doc/pre-proc.sh
@@ -3,14 +3,14 @@ version="'"$(cat version.txt)"'"
 version_date="'"$(cat version_date.txt)"'"
 if [[ -n $FC && -n $(command -v $FC) ]]; then
   if [[ $FC == *nagfor ]]; then
-    $FC -fpp -F -DPROJVERSION="${version}" -DPROJDATE="${version_date}" -o tempfile-doxygen.fpp $1 &>/dev/null
+    $FC -fpp -F -DEDKVERSION="${version}" -DEDKDATE="${version_date}" -o tempfile-doxygen.fpp $1 &>/dev/null
     cat tempfile-doxygen.fpp
     rm -f tempfile-doxygen.fpp
   else
-    $FC -cpp -E -DPROJVERSION="${version}" -DPROJDATE="${version_date}" $1
+    $FC -cpp -E -DEDKVERSION="${version}" -DEDKDATE="${version_date}" $1
   fi
 else
   if [[ -n $(command -v gfortran) ]]; then
-    gfortran -cpp -E -DPROJVERSION="${version}" -DPROJDATE="${version_date}" $1
+    gfortran -cpp -E -DEDKVERSION="${version}" -DEDKDATE="${version_date}" $1
   fi
 fi
